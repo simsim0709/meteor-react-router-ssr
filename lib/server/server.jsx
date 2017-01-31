ab7dd4e043870e1ee8df2207d66fab43c7c6d715
Ilkwon Sim <sim@theloho.com>
2017-01-31 17:54:33 +0900

    Rewrite server.jsx code

--- lib/server/server.jsx
+++ lib/server/server.jsx
@@ -6,31 +6,46 @@ import {
   createMemoryHistory,
 } from 'react-router';
 
-import SsrContext from './ssr_context';
+import ReactDOMServer from 'react-dom/server';
+// import cookieParser from 'cookie-parser';
+
+import { FastRender } from 'meteor/staringatlights:fast-render';
+import { InjectData } from 'meteor/staringatlights:inject-data';
+
+import SsrContextData from './ssr_context';
 import patchSubscribeData from './ssr_data';
 
 import {
   setCachePage,
   getCachePage,
   hasCachePage,
-  writeFromCache,
 } from './cache';
 
-import ReactDOMServer from 'react-dom/server';
-import cookieParser from 'cookie-parser';
+class ReactRouterSSR {
+  constructor() {
+    // we're stealing all the code from FlowRouter SSR
+    // https://github.com/kadirahq/flow-router/blob/ssr/server/route.js#L61
+    this.ssrContextData = new SsrContextData();
+    this.ssrContext = new Meteor.EnvironmentVariable();
+    this.inSubscription = new Meteor.EnvironmentVariable();
+    this.Run = this.Run.bind(this);
+  }
 
-const ReactRouterSSR = {
-  // creating some EnvironmentVariables that will be used later on
-  ssrContext: new Meteor.EnvironmentVariable(),
-  inSubscription: new Meteor.EnvironmentVariable(), // <-- needed in ssr_data.js
-  Run: (routes, clientOptions = {}, serverOptions = {}) => {
+  Run(routes, clientOptions = {}, serverOptions = {}) {
     // this line just patches Subscribe and find mechanisms
-    patchSubscribeData(ReactRouterSSR);
+    patchSubscribeData(this);
 
     Meteor.bindEnvironment(() => {
-      WebApp.rawConnectHandlers.use(cookieParser());
+      // WebApp.rawConnectHandlers.use(cookieParser());
 
       WebApp.connectHandlers.use(Meteor.bindEnvironment((req, res, next) => {
+        this.req = req;
+        this.res = res;
+        this.next = next;
+        this.routes = routes;
+        this.clientOptions = clientOptions;
+        this.serverOptions = serverOptions;
+
         if (!isAppUrl(req)) {
           return next();
         }
@@ -39,49 +54,137 @@ const ReactRouterSSR = {
           return next();
         }
 
-        const loginToken = undefined;
-        const headers = req.headers;
-        const context = new FastRender._Context(loginToken, { headers });
-
-        FastRender.frContext.withValue(context, () => {
-          handleRequestByReactRouter(routes, req, res, next, { clientOptions, serverOptions });
-        });
+        this.handleRequestByReactRouter();
       }));
     })();
-  },
-};
+  }
 
-export default ReactRouterSSR;
+  handleRequestByReactRouter() {
+    // const loginToken = this.req.cookies.meteor_login_token;
+    const loginToken = undefined;
+    const headers = this.req.headers;
+    const context = new FastRender._Context(loginToken, { headers });
+
+    FastRender.frContext.withValue(context, () => {
+      const history = createMemoryHistory(this.req.url);
+
+      ReactRouterMatch({
+        history,
+        routes: this.routes,
+        location: this.req.url,
+      }, Meteor.bindEnvironment((err, redirectLocation, renderProps) => {
+        console.time('ReactRouterSSR Run');
+        if (err) {
+          handleError(err, this.res);
+        } else if (redirectLocation) {
+          handleRedirect(redirectLocation, this.res);
+        } else if (renderProps) {
+          this.renderProps = renderProps;
+          this.handleSuccess();
+        } else {
+          handleNotFound(this.res);
+        }
+        console.timeEnd('ReactRouterSSR Run');
+      }));
+    });
+  }
+
+  handleSuccess() {
+    let html;
+    let frData;
+
+    if (hasCachePage(this.req.url)) {
+      const cachedPage = getCachePage(this.req.url);
+      html = cachedPage.html;
+      frData = cachedPage.frData;
+    } else {
+      const rendered = this.renderHtml();
+      html = rendered.html;
+      frData = rendered.frData;
+    }
+
+    InjectData.pushData(this.res, 'fast-render-data', frData);
+
+    this.res.write = this.patchResWrite(html);
+    this.next();
+  }
+
+  renderHtml() {
+    let html;
+    let frData;
+
+    this.ssrContext.withValue(this.ssrContextData, () => {
+      try {
+        if (!this.serverOptions.disableSSR) {
+          const app = (
+            <RouterContext
+              {...this.renderProps}
+              {...this.serverOptions.props}
+            />
+          );
+
+          html = ReactDOMServer.renderToString(app);
+          frData = this.createFrData();
+        }
+      } catch (ex) {
+        console.error(`Error when doing SSR. path:${this.req.url}: ${ex.message}`);
+        console.error(ex.stack);
+      }
+    });
+
+    setCachePage(this.req.url, { html, frData });
+
+    return { html, frData };
+  }
+
+  patchResWrite(html) {
+    const self = this;
+    const originalWrite = this.res.write;
+
+    return function (data) {
+      if (typeof data === 'string' && data.indexOf('<!DOCTYPE html>') === 0) {
+        let rootElementAttributes = '';
+        const attributes = self.clientOptions.rootElementAttributes instanceof Array ? self.clientOptions.rootElementAttributes : [];
+        if (attributes[0] instanceof Array) {
+          for (let i = 0; i < attributes.length; i++) {
+            rootElementAttributes = `${rootElementAttributes} ${attributes[i][0]}="${attributes[i][1]}"`;
+          }
+        } else if (attributes.length > 0) {
+          rootElementAttributes = ` ${attributes[0]}="${attributes[1]}"`;
+        }
+
+        data = data.replace('<body>', `<body><${self.clientOptions.rootElementType || 'div'} id="${self.clientOptions.rootElement || 'react-app'}"${rootElementAttributes}>${html}</${self.clientOptions.rootElementType || 'div'}>`);
+      }
+
+      if (self.serverOptions.shouldCache) {
+        // setCachePage(reqUrl, data);
+      }
+
+      originalWrite.call(this, data);
+    };
+  }
+
+  createFrData() {
+    const frData = InjectData.getData(this.res, 'fast-render-data');
+
+    if (frData) {
+      this.ssrContextData.addData(frData.collectionData);
+    }
+
+    // I'm pretty sure this could be avoided in a more elegant way?
+    const context = FastRender.frContext.get();
+    const data = context.getData();
+
+    return data;
+  }
+}
+
+const reactRouterSsr = new ReactRouterSSR();
+
+export default reactRouterSsr;
 
 function isUrlsDisabledSSR(urls, reqUrl) {
   return urls && Array.isArray(urls) && urls.some(url => reqUrl.startsWith(url));
-}
-
-function handleRequestByReactRouter(routes, req, res, next, { clientOptions, serverOptions }) {
-  const history = createMemoryHistory(req.url);
-
-  ReactRouterMatch({
-    history,
-    routes,
-    location: req.url,
-  }, Meteor.bindEnvironment((err, redirectLocation, renderProps) => {
-    if (err) {
-      handleError(err, res);
-    } else if (redirectLocation) {
-      handleRedirect(redirectLocation, res);
-    } else if (renderProps) {
-      handleSuccess({
-        clientOptions,
-        serverOptions,
-        req,
-        res,
-        next,
-        renderProps,
-      });
-    } else {
-      handleNotFound(res);
-    }
-  }));
 }
 
 function handleError(error, res) {
@@ -99,129 +202,6 @@ function handleNotFound(res) {
   res.writeHead(404);
   res.write('<h1>404 Not Found</h1>');
   res.end();
-}
-
-function handleSuccess({
-  userId,
-  clientOptions,
-  serverOptions,
-  req,
-  res,
-  next,
-  renderProps,
-}) {
-  if (hasCachePage(req.url)) {
-    console.log('RENDER_CACHED_PAGE');
-    const originalWrite = res.write;
-    const cachedPage = getCachePage(req.url);
-
-    res.write = function () {
-      originalWrite.call(this, cachedPage);
-    };
-  } else {
-    const html = renderHtml({
-      clientOptions,
-      serverOptions,
-      req,
-      res,
-      renderProps,
-    });
-
-    res.write = patchResWrite({
-      userId,
-      clientOptions,
-      serverOptions,
-      html,
-      originalWrite: res.write,
-      reqUrl: req.url,
-    });
-  }
-
-  next();
-}
-
-function createFrData(res, ssrContext) {
-  const frData = InjectData.getData(res, 'fast-render-data');
-  // console.log('frData', frData.collectionData);
-  if (frData) {
-    ssrContext.addData(frData.collectionData);
-  }
-
-  // I'm pretty sure this could be avoided in a more elegant way?
-  const context = FastRender.frContext.get();
-  const data = context.getData();
-  InjectData.pushData(res, 'fast-render-data', data);
-}
-
-function renderHtml({
-  clientOptions,
-  serverOptions,
-  req,
-  res,
-  renderProps,
-}) {
-  let html;
-
-  // we're stealing all the code from FlowRouter SSR
-  // https://github.com/kadirahq/flow-router/blob/ssr/server/route.js#L61
-  const ssrContext = new SsrContext();
-
-  ReactRouterSSR.ssrContext.withValue(ssrContext, () => {
-    try {
-      if (!serverOptions.disableSSR) {
-        console.time('REACT_RENDER_TO_STRING');
-
-        const app = (
-          <RouterContext
-            {...renderProps}
-            {...serverOptions.props}
-          />
-        );
-
-        html = ReactDOMServer.renderToString(app);
-
-        console.timeEnd('REACT_RENDER_TO_STRING');
-
-        createFrData(res, ssrContext);
-      }
-    } catch (ex) {
-      console.error(`Error when doing SSR. path:${req.url}: ${ex.message}`);
-      console.error(ex.stack);
-    }
-  });
-
-  return html;
-}
-
-function patchResWrite({
-  userId,
-  clientOptions,
-  serverOptions,
-  originalWrite,
-  html,
-  reqUrl,
-}) {
-  return function (data) {
-    if (typeof data === 'string' && data.indexOf('<!DOCTYPE html>') === 0) {
-      let rootElementAttributes = '';
-      const attributes = clientOptions.rootElementAttributes instanceof Array ? clientOptions.rootElementAttributes : [];
-      if (attributes[0] instanceof Array) {
-        for (let i = 0; i < attributes.length; i++) {
-          rootElementAttributes = `${rootElementAttributes} ${attributes[i][0]}="${attributes[i][1]}"`;
-        }
-      } else if (attributes.length > 0) {
-        rootElementAttributes = ` ${attributes[0]}="${attributes[1]}"`;
-      }
-
-      data = data.replace('<body>', `<body><${clientOptions.rootElementType || 'div'} id="${clientOptions.rootElement || 'react-app'}"${rootElementAttributes}>${html}</${clientOptions.rootElementType || 'div'}>`);
-    }
-
-    if (serverOptions.shouldCache) {
-      setCachePage(reqUrl, data);
-    }
-
-    originalWrite.call(this, data);
-  };
 }
 
 function isAppUrl(req) {

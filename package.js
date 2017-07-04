9df48459d57e6923a19f19bdea2e639f59a96a39
Ilkwon Sim <simsim0709@gmail.com>
2017-07-04 10:40:38 +0900

    Update version 3.2.5_3

--- package.js
+++ package.js
@@ -1,7 +1,8 @@
 Package.describe({
   name: 'thelohoadmin:react-router-ssr',
-  version: '3.2.5_1',
-  summary: 'Server-side rendering for react-router and react-meteor-data rehydratating Meteor subscriptions',
+  version: '3.2.5_3',
+  summary:
+    'Server-side rendering for react-router and react-meteor-data rehydratating Meteor subscriptions',
   git: 'https://github.com/simsim0709/meteor-react-router-ssr.git',
   documentation: 'README.md',
 });
@@ -11,7 +12,7 @@ Npm.depends({
   // 'cookie-parser': '1.4.1',
 });
 
-Package.onUse(function (api) {
+Package.onUse(function(api) {
   api.versionsFrom('1.3');
   api.use([
     'ecmascript',
@@ -22,19 +23,22 @@ Package.onUse(function (api) {
     'tmeasday:check-npm-versions@0.2.0',
   ]);
 
-  api.use([
-    'underscore@1.0.3',
-    'webapp@1.2.0',
-    'mongo@1.0.0',
-    'routepolicy@1.0.5',
-    'url@1.0.9',
-  ], 'server');
+  api.use(
+    [
+      'underscore@1.0.3',
+      'webapp@1.2.0',
+      'mongo@1.0.0',
+      'routepolicy@1.0.5',
+      'url@1.0.9',
+    ],
+    'server'
+  );
 
-  api.use([
-    'autopublish@1.0.0',
-    'tmeasday:publish-counts@0.7.0',
-    'promise@0.5.1',
-  ], 'server', { weak: true });
+  api.use(
+    ['autopublish@1.0.0', 'tmeasday:publish-counts@0.7.0', 'promise@0.5.1'],
+    'server',
+    { weak: true }
+  );
 
   api.export('ReactRouterSSR');
   api.mainModule('lib/client/react-router-ssr.js', 'client');
